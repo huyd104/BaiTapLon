b5772ba311491362f33b13e97bb6e4174a4ec7cc
Thanhdat186 <1951060598@e.tlu.edu.vn>
2022-01-06 22:21:01 +0700

    chinhsua

--- template/header.php
+++ template/header.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Hahalolo</title>
-    <script
-      src="https://kit.fontawesome.com/511b6f8ac2.js"
-      crossorigin="anonymous"
-    ></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/stylesi.css">
-  </head>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Hahalolo</title>
+  <script src="https://kit.fontawesome.com/511b6f8ac2.js" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
---
+++ template/nav.php
@@ -0,0 +1,78 @@
+<div id="header">
+    <div class="left">
+
+      <a href="https://www.hahalolo.com/"> <img class="ms-4" src="assets/img/hahalolo.png" alt=""></a>
+    </div>
+    <div class="mid d-flex ">
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="https://www.hahalolo.com/">
+          <div class="d-flex justify-content-center  "><span class="material-icons">
+              feed
+            </span></div>
+          <div class=""><span>Bảng Tin</span></div>
+        </a>
+      </div>
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="https://www.hahalolo.com/experience">
+          <div class="d-flex justify-content-center"><span class="material-icons">
+              pedal_bike
+            </span></div>
+          <div><span>Trải Nghiệm</span></div>
+        </a>
+      </div>
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="https://www.hahalolo.com/tour">
+          <div class="d-flex justify-content-center"><span class="material-icons">
+              location_on</div>
+          <div><span>Tour</span></div>
+        </a>
+      </div>
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="hotel.html">
+          <div class="d-flex justify-content-center"><span class="material-icons">
+              maps_home_work
+            </span></div>
+          <div><span>Khách Sạn</span></div>
+        </a>
+      </div>
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="https://flight.hahalolo.com/">
+          <div class="d-flex justify-content-center"><span class="material-icons">
+              flight
+            </span></div>
+          <div><span class="">Vé Máy Bay</span></div>
+        </a>
+      </div>
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="https://car.hahalolo.com/">
+          <div class="d-flex justify-content-center"><span class="material-icons">
+              directions_car
+            </span></div>
+          <div><span>Thuê Xe</span></div>
+        </a>
+      </div>
+      <div class="iconmid">
+        <a style="text-decoration: none;" href="https://www.hahalolo.com/shopping">
+          <div class="d-flex justify-content-center"><span class="material-icons">
+              add_shopping_cart
+            </span></div>
+          <div><Span>Mua Sắm</Span></div>
+        </a>
+      </div>
+
+    </div>
+    <div class="right ">
+
+      <span class="material-icons iconright1">
+        credit_card
+      </span>
+      <a href="signin.php"><span class="material-icons dn">
+          arrow_drop_down
+        </span></a>
+
+      <span class="material-icons iconright3">
+        account_circle
+      </span>
+    </div>
+
+  </div>
